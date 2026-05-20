7fb002686d61a631dc3cd53bbf5f8afdad13e4e9
Preet Rana <preetrana1263@gmail.com>
2026-05-20 16:57:12 +0530

    starting to handle input from user and print them on screen. Also implement the exit of screen

--- include/textEditor.hpp
+++ include/textEditor.hpp
@@ -3,12 +3,23 @@
 #include <string>
 #include <vector>
 
+#ifndef CTRL
+#define CTRL(c) ((c) & 0x1f)
+#endif // !DEBUG
+
+struct Cursor {
+    int x,y;
+};
+
 class TextEditor{
     std::string fileName;
     std::fstream file;
     std::vector<std::string> data;
+    Cursor c;
+    bool isOpen;
 
 public:
     TextEditor(std::string fileName);
     void openScreen();
+    void handleInput(int ch);
 };
--- src/textEditor.cpp
+++ src/textEditor.cpp
@@ -6,6 +6,9 @@
 TextEditor::TextEditor(std::string fileName){
     this->fileName = fileName;
     this->file.open(fileName, std::ios::in | std::ios::out);
+    c.x = 0;
+    c.y = 0;
+    this->isOpen = true;
 
     if(!file.is_open()){
         std::ofstream createNewFile(fileName);
@@ -19,15 +22,44 @@ TextEditor::TextEditor(std::string fileName){
         this->data.push_back(line);
     }
 
+    c.x = data[data.size()-1].size();
+    c.y = data.size() - 1;
+
     file.clear();
 }
 
 void TextEditor::openScreen(){
+    int ch;
+
     initscr();
+    raw();
+    keypad(stdscr, true);
+    noecho();
+
     for(std::string line : this->data){
         printw("%s\n",line.c_str());
     }
+
+    move(this->c.y,this->c.x);
     refresh();
-    getch();
+
+    while(this->isOpen){
+        ch = getch();
+        this->handleInput(ch);
+    }
+
     endwin();
+}
+
+void TextEditor::handleInput(int ch){
+    switch (ch) {
+        case CTRL('q'):{
+            this->isOpen = false;
+        }
+        case KEY_BACKSPACE:{
+        }
+        default:{
+            printw("%c",ch);
+        }
+    }
 }
